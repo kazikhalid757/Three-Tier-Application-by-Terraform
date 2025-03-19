2831b243bc2fe83d23a8875c2a32940782280fea
kazi khalid <kazikhalid757@gmail.com>
2025-03-19 22:21:12 +0600

    update main

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -8,6 +8,9 @@ resource "aws_db_instance" "database" {
   publicly_accessible = false
   db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  
+  skip_final_snapshot = true  # Skip final snapshot when destroying
+  final_snapshot_identifier = "final-snapshot"  # Required if skip_final_snapshot is false
 }
 
 resource "aws_security_group" "rds_sg" {
