27df62e400085719041b31581889f99e61e36f64
kazi khalid <kazikhalid757@gmail.com>
2025-03-20 23:55:12 +0600

    update

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    security_groups = [var.backend_sg_id] # Allow only EC2 to access RDS
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
